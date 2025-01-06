a3aaa03def3c54e7159b72f7a271acaaffce3e54
Arthur Mialon <arthurmialon@gmail.com>
2025-01-06 10:39:42 +0100

    chore: remove xcode setup
    Signed-off-by: Arthur Mialon <arthurmialon@gmail.com>

--- .scripts/install.sh
+++ .scripts/install.sh
@@ -15,44 +15,6 @@ reset=`tput sgr0`
 # Let's do it !
 echo "\n ${green}Setting up your Mac...${reset}"
 
-###############################################################################
-# XCode Command Line Tools
-# thanks to https://github.com/nicksp/dotfiles/blob/master/setup-new-machine.sh
-###############################################################################
-if ! xcode-select --print-path &> /dev/null; then
-
-  # Prompt user to install the XCode Command Line Tools
-  xcode-select --install &> /dev/null
-
-  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-  # Wait until the XCode Command Line Tools are installed
-  until xcode-select --print-path &> /dev/null; do
-    sleep 5
-  done
-
-  print_result $? 'Install XCode Command Line Tools'
-
-  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-  # Point the `xcode-select` developer directory to
-  # the appropriate directory from within `Xcode.app`
-  # https://github.com/alrra/dotfiles/issues/13
-
-  sudo xcode-select -switch /Applications/Xcode.app/Contents/Developer
-  print_result $? 'Make "xcode-select" developer directory point to Xcode'
-
-  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-  # Prompt user to agree to the terms of the Xcode license
-  # https://github.com/alrra/dotfiles/issues/10
-
-  sudo xcodebuild -license
-  print_result $? 'Agree with the XCode Command Line Tools licence'
-
-fi
-###############################################################################
-
 ####################################
 #  BREW
 ####################################
